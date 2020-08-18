9d06a8c93296c51d66a922a0aa46832b221e5c5f
ctross <ctross@ucdavis.edu>
2020-08-18 13:54:57 +0200

    Update Simulate_Skew.R

--- Code/Simulate_Skew.R
+++ Code/Simulate_Skew.R
@@ -51,18 +51,21 @@ Mraw_index <- function(r,t) {
            }
            }
 
-M_index <- function(r,t) {
-           R <- sum(r)
-           T <- sum(t)
-           N <- length(t)
-           
-           alpha <-  t/T
-
-           X <- rmultinom(1,sum(R),alpha)
-         
-           return(Mraw_index(r,t) - Mraw_index(X,t) )
-     
-           }
+M_index <- function (r, t, Samples = 50){
+    if (min(t) <= 0) {
+        return(NA)
+    }
+    else {
+        E_Mraw = rep(NA, Samples)
+        R = sum(r)
+        t_hat = t/sum(t)
+        for (j in 1:Samples) {
+            E_Mraw[j] <- Mraw_index(rmultinom(1, R, t_hat), t)
+          }
+        M = Mraw_index(r, t) - mean(E_Mraw)
+        return(M)
+    }
+}
 
 MMP_index <- function(r) { 
            R <- sum(r)
@@ -92,6 +95,40 @@ Lambda_index <- function(r) { # Cant really tell if this is correct, given the e
            }
 }
 
+Sigma_index <- function(r) { 
+           N <- length(r)
+           R <- sum(r)
+           if(R>0){
+            X <- N*  ( (sum(r^2) - R) /(R^2 - R)     )
+            return(X)
+            }else{
+           return(NA)
+           }
+}
+
+
+Simpson_l = function(r){
+           R <- sum(r)
+             l <- sum(r*(r-1))/(R*(R-1))
+             #l <- sum((r/R)^2)
+             return(l)
+             }
+             
+Ruzzante_Q = function(r){
+           R <- sum(r)
+             N <- length(r)
+           Q = (Simpson_l(r) - (1/N))/(1 - (1/N))
+           return(Q)
+             }
+
+Waples_Delta = function(r){
+           R <- sum(r)
+           N <- length(r)
+           D = ( var(r)/(mean(r)^2) ) - (N/R)
+           return(D)
+             }
+
+
 ########################################################### Results Calculations
 Res <- function(x){
            r <- x$r
@@ -108,6 +145,9 @@ Res <- function(x){
            d[9] <- length(t)
            d[10] <- MMP_index(r)
            d[11] <- Lambda_index(r)
+           d[12] <- Sigma_index(r)
+           d[13] <- Ruzzante_Q(r)
+           d[14] <- Waples_Delta(r)
            return(d)
            }
            
@@ -124,6 +164,9 @@ Parse <- function(Results,Lab1="A",Lab2="B",Lab3="C"){
            N <- c(Results[,,9])
            MMP <- c(Results[,,10])
            Lambda <- c(Results[,,11])
+           Sigma <- c(Results[,,12])
+           Q_index <- c(Results[,,13])
+           D_index <- c(Results[,,14])
            
            CV_dat <- data.frame(Value=CV,N=N,R=R,T=T,Rate=Rate,Index=rep("I",length(CV)))  
            rCV_dat <- data.frame(Value=rCV,N=N,R=R,T=T,Rate=Rate,Index=rep("I_rate",length(CV)))   
@@ -132,8 +175,11 @@ Parse <- function(Results,Lab1="A",Lab2="B",Lab3="C"){
            M_dat <- data.frame(Value=M,N=N,R=R,T=T,Rate=Rate,Index=rep("M",length(M))) 
            MMP_dat <- data.frame(Value=MMP,N=N,R=R,T=T,Rate=Rate,Index=rep("MMP",length(MMP))) 
            Lambda_dat <- data.frame(Value=Lambda,N=N,R=R,T=T,Rate=Rate,Index=rep("Lambda",length(Lambda))) 
+           Sigma_dat <- data.frame(Value=Sigma,N=N,R=R,T=T,Rate=Rate,Index=rep("Morisita",length(Sigma))) 
+           Q_dat <- data.frame(Value=Q_index,N=N,R=R,T=T,Rate=Rate,Index=rep("Q",length(Q_index))) 
+           D_dat <- data.frame(Value=D_index,N=N,R=R,T=T,Rate=Rate,Index=rep("Delta_I",length(D_index))) 
   
-           dat <- rbind(CV_dat, rCV_dat, B_dat, Mraw_dat, M_dat, MMP_dat, Lambda_dat)
+           dat <- rbind(CV_dat, rCV_dat, B_dat, Mraw_dat, M_dat, MMP_dat, Lambda_dat, Sigma_dat, Q_dat, D_dat)
            dat$Value <- ifelse(dat$Value==Inf | dat$Value== -Inf,NA,dat$Value)
            dat$Lab1 <- rep(Lab1,length(dat$Value))
            dat$Lab2 <- rep(Lab2,length(dat$Value))
@@ -141,57 +187,124 @@ Parse <- function(Results,Lab1="A",Lab2="B",Lab3="C"){
            return(dat)
            }
 
-############################################################## Run Simulation, N
- S <- 500  # Samples
- Q <- 30   # Diferences
+############################################################## First make basic insets
+set.seed(1)
+skew1 <- sim_rs(N=50000,Rate=7.0, L=1, U=1,et=1,es=0.01)
+skew2 <- sim_rs(N=50000,Rate=7.0, L=1, U=1,et=1,es=0.31)
+skew3 <- sim_rs(N=50000,Rate=7.0, L=1, U=1,et=1,es=0.61)
+dfskew <- data.frame(RS=c(skew1$r,skew2$r,skew3$r), Skew=rep(c("Low skew","Mid skew","High skew"),each=50000))
+dfskew$Skew = factor(dfskew$Skew,levels(dfskew$Skew)[c(1,3,2)])
+
+mu <- ddply(dfskew, "Skew", summarise, grp.mean=mean(RS))
+
+p <- ggplot(dfskew, aes(x=RS, fill=Skew, color=NULL)) +
+     geom_histogram(position="identity", alpha=0.5, binwidth=1)+
+     #geom_vline(data=mu, aes(xintercept=grp.mean, color=Skew), linetype="dashed") +
+     scale_color_manual(values=c("black","darkgoldenrod","steelblue")) + scale_fill_manual(values=c("black","darkgoldenrod","steelblue")) + coord_cartesian(xlim = c(0, 80)) +
+     theme(strip.text.x = element_text(size=14, face="bold"), strip.text.y = element_text(size=14, face="bold")) + theme(axis.text=element_text(size=12),
+      axis.title=element_text(size=14,face="bold"))+  theme(legend.position="bottom", legend.box = "vertical",legend.title = element_blank()) +
+     theme(legend.text=element_text(size=12))+guides(fill = guide_legend(order=1),color = guide_legend(order=1),
+      lty = guide_legend(order=2))+ guides(lty = guide_legend(override.aes = list(col = 'black'))) +
+       theme(axis.text.y=element_blank(), axis.ticks.y=element_blank()) + ylab("Probability mass") + xlab("Reproductive success")
+p
+ggsave("SimResNB.pdf",p,height=4.5,width=4.5)
+
+############################################################## First make basic insets, scatter
+S <- 500  # Samples
+Q <- 30   # Diferences
  
  Pop <- round(exp(seq(2.2,7,length.out=Q)),0) 
- Res1 <- array(NA,c(S,Q,11))
- Res2 <- array(NA,c(S,Q,11))
- Res3 <- array(NA,c(S,Q,11))
- Res4 <- array(NA,c(S,Q,11))
- Res5 <- array(NA,c(S,Q,11))
- Res6 <- array(NA,c(S,Q,11))
- Res7 <- array(NA,c(S,Q,11))
- Res8 <- array(NA,c(S,Q,11))
- Res9 <- array(NA,c(S,Q,11))
+ Res1a <- array(NA,c(S,Q,14))
+ Res2a <- array(NA,c(S,Q,14))
+ Res3a <- array(NA,c(S,Q,14))
+  
+for(s in 1:S){
+ for(q in 1:Q){
+ Res1a[s,q,] <- Res(sim_rs(N=Pop[q],Rate=7.0, L=1, U=1,et=1,es=0.01))
+ Res2a[s,q,] <- Res(sim_rs(N=Pop[q],Rate=7.0, L=1, U=1,et=1,es=0.31))
+ Res3a[s,q,] <- Res(sim_rs(N=Pop[q],Rate=7.0, L=1, U=1,et=1,es=0.61))
+              }
+              }
+                
+base_breaks <- function(n = 10){
+    function(x) {
+        axisTicks(log10(range(x, na.rm = TRUE)), log = TRUE, n = n)
+    }
+}
+
+              
+ dat1a <- rbind(Parse(Res1a, "Low Skew", "Mid Mean", "Equal Exposure"),Parse(Res2a, "Mid Skew", "Mid Mean", "Equal Exposure"),Parse(Res3a, "High Skew", "Mid Mean", "Equal Exposure"))
+              
+colnames(dat1a) <- c("Value", "N", "R", "T", "Rate", "Index", "Skew", "Mean", "Exposure")
+dat1a <- dat1a[which(dat1a$Index=="M" | dat1a$Index=="B"),]
+dat1a$Skew = factor(dat1a$Skew)
+dat1a$Skew = factor(dat1a$Skew,levels(dat1a$Skew)[c(1,3,2)])
+
+jitter <- position_jitter(width = 0.01, height = 0.01)
+
+p2 <- ggplot(dat1a, aes(N, Value)) +
+  geom_point(aes(color=Skew),position = jitter, alpha=0.05) + facet_grid(Index~ . ,scales="free_y") +
+  geom_smooth(aes(color=Skew),span = 0.3) + scale_x_continuous(trans='log10',breaks = base_breaks(n=9)) +
+     scale_color_manual(values=c("black","darkgoldenrod","steelblue")) + scale_fill_manual(values=c("black","darkgoldenrod","steelblue")) + 
+     theme(strip.text.x = element_text(size=14, face="bold"), strip.text.y = element_text(size=14, face="bold")) + theme(axis.text=element_text(size=12),
+      axis.title=element_text(size=14,face="bold"))+  theme(legend.position="bottom", legend.box = "vertical",legend.title = element_blank()) +
+     theme(legend.text=element_text(size=12))+guides(fill = guide_legend(order=1),color = guide_legend(order=1),
+      lty = guide_legend(order=2))+ guides(lty = guide_legend(override.aes = list(col = 'black')))  + ylab("Index Value")
+ p2 
+ggsave("SimResFocus.pdf",p2,height=9,width=4.5)
+
+############################################################## Run Full Simulation, N
+ S <- 500  # Samples
+ Q <- 30   # Diferences
+ set.seed(1)
  
- Res1b <- array(NA,c(S,Q,11))
- Res2b <- array(NA,c(S,Q,11))
- Res3b <- array(NA,c(S,Q,11))
- Res4b <- array(NA,c(S,Q,11))
- Res5b <- array(NA,c(S,Q,11))
- Res6b <- array(NA,c(S,Q,11))
- Res7b <- array(NA,c(S,Q,11))
- Res8b <- array(NA,c(S,Q,11))
- Res9b <- array(NA,c(S,Q,11))
+ Pop <- round(exp(seq(2.2,7,length.out=Q)),0) 
+ Res1 <- array(NA,c(S,Q,14))
+ Res2 <- array(NA,c(S,Q,14))
+ Res3 <- array(NA,c(S,Q,14))
+ Res4 <- array(NA,c(S,Q,14))
+ Res5 <- array(NA,c(S,Q,14))
+ Res6 <- array(NA,c(S,Q,14))
+ Res7 <- array(NA,c(S,Q,14))
+ Res8 <- array(NA,c(S,Q,14))
+ Res9 <- array(NA,c(S,Q,14))
+ 
+ Res1b <- array(NA,c(S,Q,14))
+ Res2b <- array(NA,c(S,Q,14))
+ Res3b <- array(NA,c(S,Q,14))
+ Res4b <- array(NA,c(S,Q,14))
+ Res5b <- array(NA,c(S,Q,14))
+ Res6b <- array(NA,c(S,Q,14))
+ Res7b <- array(NA,c(S,Q,14))
+ Res8b <- array(NA,c(S,Q,14))
+ Res9b <- array(NA,c(S,Q,14))
  
 for(s in 1:S){
  for(q in 1:Q){
  Res1[s,q,] <- Res(sim_rs(N=Pop[q],Rate=1.0, L=1, U=1,et=1,es=0.01))
- Res2[s,q,] <- Res(sim_rs(N=Pop[q],Rate=1.0, L=1, U=1,et=1,es=0.39))
- Res3[s,q,] <- Res(sim_rs(N=Pop[q],Rate=1.0, L=1, U=1,et=1,es=0.79))
+ Res2[s,q,] <- Res(sim_rs(N=Pop[q],Rate=1.0, L=1, U=1,et=1,es=0.31))
+ Res3[s,q,] <- Res(sim_rs(N=Pop[q],Rate=1.0, L=1, U=1,et=1,es=0.61))
 
  Res4[s,q,] <- Res(sim_rs(N=Pop[q],Rate=7.0, L=1, U=1,et=1,es=0.01))
- Res5[s,q,] <- Res(sim_rs(N=Pop[q],Rate=7.0, L=1, U=1,et=1,es=0.39))
- Res6[s,q,] <- Res(sim_rs(N=Pop[q],Rate=7.0, L=1, U=1,et=1,es=0.79))
+ Res5[s,q,] <- Res(sim_rs(N=Pop[q],Rate=7.0, L=1, U=1,et=1,es=0.31))
+ Res6[s,q,] <- Res(sim_rs(N=Pop[q],Rate=7.0, L=1, U=1,et=1,es=0.61))
  
  Res7[s,q,] <- Res(sim_rs(N=Pop[q],Rate=20.0, L=1, U=1,et=1,es=0.01))
- Res8[s,q,] <- Res(sim_rs(N=Pop[q],Rate=20.0, L=1, U=1,et=1,es=0.39))
- Res9[s,q,] <- Res(sim_rs(N=Pop[q],Rate=20.0, L=1, U=1,et=1,es=0.79))
+ Res8[s,q,] <- Res(sim_rs(N=Pop[q],Rate=20.0, L=1, U=1,et=1,es=0.31))
+ Res9[s,q,] <- Res(sim_rs(N=Pop[q],Rate=20.0, L=1, U=1,et=1,es=0.61))
 
 
  Res1b[s,q,] <- Res(sim_rs(N=Pop[q],Rate=1.0, L=0.2, U=1,et=1,es=0.01))
- Res2b[s,q,] <- Res(sim_rs(N=Pop[q],Rate=1.0, L=0.2, U=1,et=1,es=0.39))
- Res3b[s,q,] <- Res(sim_rs(N=Pop[q],Rate=1.0, L=0.2, U=1,et=1,es=0.79))
+ Res2b[s,q,] <- Res(sim_rs(N=Pop[q],Rate=1.0, L=0.2, U=1,et=1,es=0.31))
+ Res3b[s,q,] <- Res(sim_rs(N=Pop[q],Rate=1.0, L=0.2, U=1,et=1,es=0.61))
 
  Res4b[s,q,] <- Res(sim_rs(N=Pop[q],Rate=7.0, L=0.2, U=1,et=1,es=0.01))
- Res5b[s,q,] <- Res(sim_rs(N=Pop[q],Rate=7.0, L=0.2, U=1,et=1,es=0.39))
- Res6b[s,q,] <- Res(sim_rs(N=Pop[q],Rate=7.0, L=0.2, U=1,et=1,es=0.79))
+ Res5b[s,q,] <- Res(sim_rs(N=Pop[q],Rate=7.0, L=0.2, U=1,et=1,es=0.31))
+ Res6b[s,q,] <- Res(sim_rs(N=Pop[q],Rate=7.0, L=0.2, U=1,et=1,es=0.61))
  
  Res7b[s,q,] <- Res(sim_rs(N=Pop[q],Rate=20.0, L=0.2, U=1,et=1,es=0.01))
- Res8b[s,q,] <- Res(sim_rs(N=Pop[q],Rate=20.0, L=0.2, U=1,et=1,es=0.39))
- Res9b[s,q,] <- Res(sim_rs(N=Pop[q],Rate=20.0, L=0.2, U=1,et=1,es=0.79))
+ Res8b[s,q,] <- Res(sim_rs(N=Pop[q],Rate=20.0, L=0.2, U=1,et=1,es=0.31))
+ Res9b[s,q,] <- Res(sim_rs(N=Pop[q],Rate=20.0, L=0.2, U=1,et=1,es=0.61))
               }
                 }
                 
@@ -240,16 +353,19 @@ dat$Skew <- factor(dat$Skew)
 dat$Skew <- factor(dat$Skew,levels(dat$Skew)[c(1,3,2)])
 
 dat$Index <- factor(dat$Index)
-dat$Index <- factor(dat$Index,levels(dat$Index)[c(6,7,3,1,2,4,5)])
+dat$Index <- factor(dat$Index,levels(dat$Index)[c(6,7,9,3,1,2,4,8,10,5)])
+
+hline_dat = data.frame(Index=levels(dat$Index),
+                       threshold=c(0.5, 0.5, 0.12, 0.12, 2, 2, 2, 2.25, 1.5, 1.5))
 
  (p1<- ggplot(dat, aes(N, Value, color=Mean, fill=Mean, lty=Exposure))+
-  # geom_point()+
+  geom_hline(data=hline_dat, aes(yintercept=threshold), colour="grey92") +
   stat_smooth()+
   facet_grid(Index~Skew,scales="free_y")  +  ylab("Index Value")+ xlab("Sample Size")
   + scale_x_continuous(trans='log10',breaks = base_breaks(n=9)) 
   +   theme(strip.text.x = element_text(size=14, face="bold"),
           strip.text.y = element_text(size=14, face="bold")) + theme(axis.text=element_text(size=12),
-        axis.title=element_text(size=14,face="bold"))+ 
+          axis.title=element_text(size=14,face="bold"))+ 
           scale_color_viridis(discrete=TRUE,end=0.8, option="inferno",direction=-1)+ 
           scale_fill_viridis(discrete=TRUE,end=0.8, option="inferno",direction=-1) +
           theme(legend.position="bottom", legend.box = "vertical",legend.title = element_blank()) +
@@ -257,7 +373,7 @@ dat$Index <- factor(dat$Index,levels(dat$Index)[c(6,7,3,1,2,4,5)])
          lty = guide_legend(order=2))+ guides(lty = guide_legend(override.aes = list(col = 'black')))
                                  )
   
-ggsave("SimRes.pdf",p1,height=11.5,width=8.5)
+ggsave("SimRes.pdf",p1,height=12.5,width=8.5)
 
 
 
